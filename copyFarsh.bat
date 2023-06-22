5249e543427721dfb17f3e57aab9945097f0f78a
Mohamed Shalaby <ctybrd@gmail.com>
2023-06-22 03:50:04 +0300

    bat

---
+++ copyFarsh.bat
@@ -0,0 +1,6 @@
+COPY E:\Qeraat\farsh_v4.db E:\Qeraat\Wursha_QuranHolder\other\data\ /Y
+COPY E:\Qeraat\farsh_v4.db E:\Qeraat\Wursha_QuranHolder\platforms\android\app\build\intermediates\assets\debug\mergeDebugAssets\www\ /Y
+COPY E:\Qeraat\farsh_v4.db E:\Qeraat\Wursha_QuranHolder\platforms\android\app\src\main\assets\www /Y
+COPY E:\Qeraat\farsh_v4.db E:\Qeraat\Wursha_QuranHolder\www\ /Y
+
+
