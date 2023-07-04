176b1e2ec3098f8a0a7a2b3bf584819d25586481
Mohamed Shalaby <ctybrd@gmail.com>
2023-07-05 00:49:45 +0300

    TayseerOutput.py

--- TayseerOutput.py
+++ TayseerOutput.py
@@ -1,10 +1,9 @@
 import os
 from docx import Document
-from docx.shared import Inches
+from docx.shared import Inches, Pt
 import sqlite3
 from docx.enum.section import WD_ORIENTATION
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from itertools import islice
 
 
 def create_word_document(comments_table):
@@ -16,23 +15,20 @@ def create_word_document(comments_table):
 
     # Iterate over each page and add the corresponding image, comments, and separator
     for page_number, image_file in enumerate(image_files, start=1):
-        # Add even page with image to the left
+        section = doc.sections[-1] if doc.sections else doc.add_section()
+        section.orientation = WD_ORIENTATION.PORTRAIT
+        section.page_width = Inches(8.5)
+        section.page_height = Inches(11)
+
+        # Add image to the page
+        paragraph = doc.add_paragraph()
+        run = paragraph.add_run()
+        run.add_picture(os.path.join(image_folder, image_file), width=Inches(3))
+
         if page_number % 2 == 0:
-            section = doc.sections[-1] if doc.sections else doc.add_section()
-            section.orientation = WD_ORIENTATION.LANDSCAPE
-            section.page_width = Inches(11)
-            section.page_height = Inches(8.5)
-
-            header = section.header
-            header.is_linked_to_previous = False
-            header_first_page = header.paragraphs[0]
-            header_first_page.text = f'Page {page_number}'
-            header_first_page.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-
-            doc.add_picture(os.path.join(image_folder, image_file), width=Inches(5))
-
-        # Add page number
-        doc.add_paragraph(f'Page {page_number}')
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+        else:
+            paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
 
         # Retrieve comments from the table for the current page
         page_comments = comments_table.get(page_number, [])
@@ -40,16 +36,26 @@ def create_word_document(comments_table):
         # Add comments to the document
         for i, comment in enumerate(page_comments, start=1):
             if comment:  # Add comment only if it's not empty
-                cleaned_comment = comment.replace('\n', '. ')
-                doc.add_paragraph(f'({i}): {cleaned_comment}')
+                cleaned_comment = comment.replace('\n\r', ' ـ ')
+                cleaned_comment = cleaned_comment.replace('\r', ' ـ ')
+                cleaned_comment = cleaned_comment.replace('\n', ' ـ ')
+                paragraph = doc.add_paragraph()
+                run = paragraph.add_run()
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+                run.text = 'م'+str(i)+' - ' + cleaned_comment
+                paragraph.paragraph_format.line_spacing = Pt(12)
+
 
         # Add separator after each comment
         doc.add_paragraph()
+        doc.add_page_break()
 
-        # Add odd page with image to the right
-        if page_number % 2 != 0:
-            doc.add_picture(os.path.join(image_folder, image_file), width=Inches(5))
-            doc.add_page_break()
+    # Set consistent page margins for all sections
+    for section in doc.sections:
+        section.left_margin = Inches(1.27)
+        section.right_margin = Inches(1.27)
+        section.top_margin = Inches(1.27)
+        section.bottom_margin = Inches(1.27)
 
     # Save the document
     doc.save('output.docx')
@@ -58,7 +64,7 @@ def create_word_document(comments_table):
 def read_comments_from_database():
     conn = sqlite3.connect('comments.db')
     c = conn.cursor()
-    c.execute("SELECT page_number, comment FROM comments ORDER BY page_number")
+    c.execute("SELECT page_number, comment FROM comments where (comment is not null) and (comment <>'') ORDER BY page_number")
     rows = c.fetchall()
     comments_table = {}
     for row in rows:
