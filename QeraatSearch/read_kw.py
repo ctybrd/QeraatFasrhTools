f634cf140a2d3fe38ffd719fec8b9d62df387762
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-06-30 14:03:04 +0300

    Update read_kw.py

--- QeraatSearch/read_kw.py
+++ QeraatSearch/read_kw.py
@@ -1,26 +1,31 @@
-@ -0,0 +1,36 @@
 import json
 
 # Path to the JSON file
-file_path = 'D:/Qeraat/QeraatFasrhTools_Data/Ten_Readings/json/Qeraat_303.json'
-
-# Characters to be removed
-chars_to_remove = "ﮎﮍﭜﮖﭝsﭨ﴿﴾ﭶﮅﮗﮘﯺﮔﯡﰃ"
-
-# Create a translation table
-translation_table = str.maketrans('', '', chars_to_remove)
-
-# Function to extract text from chars and remove unwanted characters
+file_path = 'e:/Qeraat/QeraatFasrhTools_Data/Ten_Readings/json/Qeraat_118.json'
 def extract_text_from_chars(chars):
-    text = ''.join([char['unicode'] for char in chars])
-    return text #.translate(translation_table)
+    return ''.join([char['unicode'] for char in chars])
+def transliterate_number(number):
+    mapping = {
+        '٠': '0',
+        '١': '1',
+        '٢': '2',
+        '٣': '3',
+        '٤': '4',
+        '٥': '5',
+        '٦': '6',
+        '٧': '7',
+        '٨': '8',
+        '٩': '9',
+    }
+    return ''.join(mapping.get(char, char) for char in str(number))
+
 
 # Read and process the JSON file
 with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
     for item in data:
-        word_text = item['word_text'].translate(translation_table)
+        word_text = item['word_text']
         print(f"الكلمة: {word_text}")
         
         detail_text = extract_text_from_chars(item['detail_title_chars'])
@@ -32,6 +37,6 @@ with open(file_path, 'r', encoding='utf-8') as file:
             reading_detail_text = extract_text_from_chars(reading['detail_chars'])
             print(f"ترتيب: {reading_order}")
             #to print the audio file path print(f"File Path: {file_path}")
-            reading_detail_text = reading_detail_text.replace('ابنعامر', 'ابن عامر').replace('أبوجعفر', 'أبو جعفر').replace('أبوعمرو', 'أبو عمرو')
+            reading_detail_text = reading_detail_text.replace('ابنsعامر', 'ابن عامر').replace('أبوsجعفر', 'أبو جعفر').replace('أبوsعمرو', 'أبو عمرو')
             print(f"الفرش: {reading_detail_text}")
         print("\n" + "-"*50 + "\n")
