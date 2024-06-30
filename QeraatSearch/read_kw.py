1fbdc34ab93c7fccad6a4e7ea0420de7ecb51995
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-06-30 14:39:01 +0300

    Update read_kw.py

--- QeraatSearch/read_kw.py
+++ QeraatSearch/read_kw.py
@@ -1,7 +1,12 @@
 import json
+import os
 
 # Path to the JSON file
-file_path = 'e:/Qeraat/QeraatFasrhTools_Data/Ten_Readings/json/Qeraat_118.json'
+script_path = os.path.abspath(__file__)
+drive, _ = os.path.splitdrive(script_path) 
+drive = drive +'/'
+
+file_path = os.path.join(drive,'Qeraat/QeraatFasrhTools_Data/Ten_Readings/json/Qeraat_118.json')
 def extract_text_from_chars(chars):
     return ''.join([char['unicode'] for char in chars])
 def transliterate_number(number):
@@ -19,24 +24,25 @@ def transliterate_number(number):
     }
     return ''.join(mapping.get(char, char) for char in str(number))
 
-
 # Read and process the JSON file
 with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
     for item in data:
-        word_text = item['word_text']
-        print(f"الكلمة: {word_text}")
+        sub_subject = item['word_text']
+        print(f"SUB_SUBJECT: {sub_subject}")
         
-        detail_text = extract_text_from_chars(item['detail_title_chars'])
-        print(f"ملاحظة: {detail_text}")
+        notes = extract_text_from_chars(item['detail_title_chars'])
+        notes = transliterate_number(notes)
+        print(f"Notes: {notes}")
         
-        for reading in item['quran_ten_word_mp3']:
-            reading_order = reading['reading_order']
-            file_path = reading['file']
-            reading_detail_text = extract_text_from_chars(reading['detail_chars'])
-            print(f"ترتيب: {reading_order}")
+        for row in item['quran_ten_word_mp3']:
+            id = row['reading_order']
+            file_path = row['file']
+            reading = extract_text_from_chars(row['detail_chars'])
+            print(f"ID: {id}")
             #to print the audio file path print(f"File Path: {file_path}")
-            reading_detail_text = reading_detail_text.replace('ابنsعامر', 'ابن عامر').replace('أبوsجعفر', 'أبو جعفر').replace('أبوsعمرو', 'أبو عمرو')
-            print(f"الفرش: {reading_detail_text}")
-        print("\n" + "-"*50 + "\n")
+            reading = reading.replace('ابنsعامر', 'ابن عامر').replace('أبوsجعفر', 'أبو جعفر').replace('أبوsعمرو', 'أبو عمرو')
+            
+            print(f"Reading: {reading}")
+        # print("\n" + "-"*50 + "\n")
