7fbc00dca6397ed6d2c56fd173828d9459bb02c3
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-06-30 17:27:19 +0300

    add table

--- QeraatSearch/read_kw.py
+++ QeraatSearch/read_kw.py
@@ -1,7 +1,5 @@
 import json
 import os
-
-# Path to the JSON file
 script_path = os.path.abspath(__file__)
 drive, _ = os.path.splitdrive(script_path) 
 drive = drive +'/'
@@ -10,30 +8,14 @@ file_path = os.path.join(drive,'Qeraat/QeraatFasrhTools_Data/Ten_Readings/json/Q
 def extract_text_from_chars(chars):
     return ''.join([char['unicode'] for char in chars])
 def transliterate_number(number):
-    mapping = {
-        '٠': '0',
-        '١': '1',
-        '٢': '2',
-        '٣': '3',
-        '٤': '4',
-        '٥': '5',
-        '٦': '6',
-        '٧': '7',
-        '٨': '8',
-        '٩': '9',
-    }
+    mapping = {'٠':'0','١':'1','٢':'2','٣':'3','٤':'4','٥':'5','٦':'6','٧':'7', '٨':'8', '٩':'9',}
     return ''.join(mapping.get(char, char) for char in str(number))
-
-# Read and process the JSON file
 with open(file_path, 'r', encoding='utf-8') as file:
     data = json.load(file)
-
     for item in data:
         sub_subject = item['word_text']
         print(f"SUB_SUBJECT: {sub_subject}")
-        
-        notes = extract_text_from_chars(item['detail_title_chars'])
-        notes = transliterate_number(notes)
+        notes = transliterate_number(extract_text_from_chars(item['detail_title_chars']))
         print(f"Notes: {notes}")
         
         for row in item['quran_ten_word_mp3']:
@@ -42,7 +24,5 @@ with open(file_path, 'r', encoding='utf-8') as file:
             reading = extract_text_from_chars(row['detail_chars'])
             print(f"ID: {id}")
             #to print the audio file path print(f"File Path: {file_path}")
-            reading = reading.replace('ابنsعامر', 'ابن عامر').replace('أبوsجعفر', 'أبو جعفر').replace('أبوsعمرو', 'أبو عمرو')
-            
+            reading = reading.replace('ابنsعامر', 'ابن عامر').replace('أبوsجعفر', 'أبو جعفر').replace('أبوsعمرو', 'أبو عمرو')           
             print(f"Reading: {reading}")
-        # print("\n" + "-"*50 + "\n")
