b0dcf1a5f516e769fa9b7dbdbbf1afecbff14cc4
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-07-26 01:27:49 +0300

    updates

---
+++ QeraatSearch/replace_text_in_books.py
@@ -0,0 +1,62 @@
+import sqlite3
+import shutil
+import os
+
+# Copy data_v17.db from the source folder to the destination folder
+script_path = os.path.abspath(__file__)
+drive, _ = os.path.splitdrive(script_path)
+drive = drive + '/'
+
+source_db_path = drive + r'Qeraat/Wursha_QuranHolder/other/data/data_v17.db'
+destination_db_path = drive + r'Qeraat/QeraatFasrhTools/QeraatSearch/data_v17.db'
+shutil.copyfile(source_db_path, destination_db_path)
+
+# List of table names to update
+table_names = [
+    "book_qqalon",
+    "book_qwarsh",
+    "book_qibnkather",
+    "book_aboamro",
+    "book_ibnamer",
+    "book_sho3ba",
+    "book_qhamza",
+    "book_kisai",
+    "book_abujafar",
+    "book_yaqob",
+    "book_khalaf",
+    "book_all10"
+]
+
+# Connect to the database and update the text in the specified tables
+conn_data_v17 = sqlite3.connect(destination_db_path)
+cursor1 = conn_data_v17.cursor()
+for table in table_names:
+    sql = "UPDATE " + table + " SET text = REPLACE(text, 'ـ', '')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, '  ', ' ')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, '  ', ' ')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, '  ', ' ')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, '.', '')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, 'مع قصر مد البدل', '')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, 'بالفتح وقفا', 'بالفتح م وقفا')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+    sql = "UPDATE " + table + " SET text = REPLACE(text, 'بالفتح وصلا', 'بالفتح م وصلا')"
+    cursor1.execute(sql)
+    conn_data_v17.commit()
+
+
+# Commit the changes and close the connection
+conn_data_v17.commit()
+conn_data_v17.close()
