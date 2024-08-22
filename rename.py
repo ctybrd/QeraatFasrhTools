8bc52a8753ede637e971c3f6dc89a011e6f9cd7a
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-08-23 02:12:01 +0300

    libya

--- rename.py
+++ rename.py
@@ -1,11 +1,17 @@
 import os
 
-directory = 'E:\ShmrlySides\SideW'  # Replace with the path to your directory
+directory = 'D:/QPages1'  # Replace with the path to your directory
 
-# Iterate over all files in the directory
-for filename in os.listdir(directory):
-    if filename.endswith('.png'):  # Replace '.txt' with the file extension you're working with
-        new_filename = filename.lstrip('0')  # Remove leading zeros
-        original_path = os.path.join(directory, filename)
-        new_path = os.path.join(directory, new_filename)
+# Loop through the range of numbers you want to rename
+for i in range(2, 604):  # From 2 to 603 inclusive
+    original_filename = f'{i}.png'
+    new_filename = f'{i-1}.png'
+
+    original_path = os.path.join(directory, original_filename)
+    new_path = os.path.join(directory, new_filename)
+
+    # Check if the original file exists before renaming
+    if os.path.exists(original_path):
         os.rename(original_path, new_path)
+    else:
+        print(f"File {original_filename} does not exist and will be skipped.")
