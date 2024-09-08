42903692d7954008a2fe8eaff95b5a74e75ef0e6
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-09-08 23:52:27 +0300

    tags

--- QeraatSearch/tagsupdates.sql
+++ QeraatSearch/tagsupdates.sql
@@ -44,3 +44,57 @@ update quran_data set done=1 where reading
 IN (
     'بضم ميم الجمع، ووصلها بواو لفظية مع الإشباع، مع ثلاثة البدل.'
 );
+
+
+UPDATE quran_data set tags=IFNULL(tags,',') ||'idghamn,'
+where reading = 'بإدغام بلا غنة'
+and 
+ifnull(tags,',') not like '%idghamn,%';
+
+update quran_data set done=1 where reading = 'بإدغام بلا غنة'
+
+UPDATE quran_data set tags=IFNULL(tags,',') ||'nakl,'
+where reading = 'بالنقل.'
+and 
+ifnull(tags,',') not like '%nakl,%';
+
+update quran_data set done=1 where reading = 'بالنقل.'
+
+
+UPDATE quran_data set tags=IFNULL(tags,',') ||'waqfh1,'
+where reading 
+IN (
+    'وقف بتسهيل وتحقيق الهمزة.',
+    'بترك السكت وصلا، ووقف بالنقل والتحقيق.',
+    'بالسكت وعدمه وصلا، ووقف بالسكت، والنقل، وتركهما.',
+    'بالسكت وعدمه وصلا، ووقف بالسكت والنقل.',
+   'بالسكت وصلا، ووقف بالسكت والنقل.'
+);
+
+and 
+ifnull(tags,',') not like '%waqfh1,%';
+
+update quran_data set done=1 where reading 
+IN (
+    'وقف بتسهيل وتحقيق الهمزة.',
+    'بترك السكت وصلا، ووقف بالنقل والتحقيق.',
+    'بالسكت وعدمه وصلا، ووقف بالسكت، والنقل، وتركهما.',
+    'بالسكت وعدمه وصلا، ووقف بالسكت والنقل.',
+    'بالسكت وصلا، ووقف بالسكت والنقل.'
+    );
+
+
+UPDATE quran_data SET
+tags=(SELECT tags from tagsmap where tagsmap.reading=quran_data.reading),done=1
+where reading in(SELECT reading from tagsmap)
+
+
+UPDATE quran_data set tags='wafh1,',done=1 WHERE
+reading='وقف بتسهيل وتحقيق الهمزة.' 
+
+UPDATE  quran_data set tags= ',' || tags ||','
+where tags is not null;
+
+UPDATE  quran_data set tags= replace(tags,',,',',')
+where tags is not null;
+
