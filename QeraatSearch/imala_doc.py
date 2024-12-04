fd13208f8f0ca1d7c765bb4f6f15124269e0a188
Mohamed Shalaby <41000506+ctybrd@users.noreply.github.com>
2024-12-04 16:01:03 +0200

    Update imala_doc.py

--- QeraatSearch/imala_doc.py
+++ QeraatSearch/imala_doc.py
@@ -4,7 +4,7 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 import sqlite3
 
 # Connect to the SQLite database
-db_path = "d:\\Qeraat\\QeraatFasrhTools\\QeraatSearch\\qeraat_data_simple.db"
+db_path = "e:\\Qeraat\\QeraatFasrhTools\\QeraatSearch\\qeraat_data_simple.db"
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
@@ -79,65 +79,21 @@ sql_query ="""
 WITH summed_data AS (
     SELECT 
         page_shmrly,
-        CASE WHEN tags LIKE '%imalah%' THEN 
 		' إمالة هاء التأنيث وقفا (الكسائي)'
-		ELSE 
-		'' 
-		END AS description,
+		AS description,
         CASE WHEN reading LIKE '%بخلف%' THEN ' ـ بخلف ـ' ELSE '' END AS kholf,
         IFNULL(resultnew, sub_subject1) AS sub_subject,
-        --CASE WHEN rasaya IS NOT NULL THEN 'رؤوس الآي' ELSE 'ما ليس برأس آية' END AS rasaya,
-		'' as rasaya,
-        SUM(q1) AS q1, SUM(r1_1) AS r1_1, SUM(r1_2) AS r1_2,
-        SUM(q2) AS q2, SUM(r2_1) AS r2_1, SUM(r2_2) AS r2_2,
-        SUM(q3) AS q3, SUM(r3_1) AS r3_1, SUM(r3_2) AS r3_2,
-        SUM(q4) AS q4, SUM(r4_1) AS r4_1, SUM(r4_2) AS r4_2,
-        SUM(q5) AS q5, SUM(r5_1) AS r5_1, SUM(r5_2) AS r5_2,
-        SUM(q6) AS q6, SUM(r6_1) AS r6_1, SUM(r6_2) AS r6_2,
-        SUM(q7) AS q7, SUM(r7_1) AS r7_1, SUM(r7_2) AS r7_2,
-        SUM(q8) AS q8, SUM(r8_1) AS r8_1, SUM(r8_2) AS r8_2,
-        SUM(q9) AS q9, SUM(r9_1) AS r9_1, SUM(r9_2) AS r9_2,
-        SUM(q10) AS q10, SUM(r10_1) AS r10_1, SUM(r10_2) AS r10_2
+
+		'' as rasaya
     FROM quran_data
     WHERE tags LIKE '%,imalah,%' 
     GROUP BY page_shmrly, description, kholf, sub_subject, rasaya
+	ORDER by aya_index,id
 )
-SELECT page_shmrly,description,kholf,group_concat(' ﴾'|| sub_subject || '﴿ ') sub_subject,rasaya,
-    TRIM(
-        CASE WHEN q1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q1') || ', ' ELSE '' END ||
-        CASE WHEN q1 IS NULL AND r1_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R1_1') || ', ' ELSE '' END ||
-        CASE WHEN q1 IS NULL AND r1_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R1_2') || ', ' ELSE '' END ||
-        CASE WHEN q2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q2') || ', ' ELSE '' END ||
-        CASE WHEN q2 IS NULL AND r2_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R2_1') || ', ' ELSE '' END ||
-        CASE WHEN q2 IS NULL AND r2_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R2_2') || ', ' ELSE '' END ||
-        CASE WHEN q3 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q3') || ', ' ELSE '' END ||
-        CASE WHEN q3 IS NULL AND r3_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R3_1') || ', ' ELSE '' END ||
-        CASE WHEN q3 IS NULL AND r3_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R3_2') || ', ' ELSE '' END ||
-        CASE WHEN q4 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q4') || ', ' ELSE '' END ||
-        CASE WHEN q4 IS NULL AND r4_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R4_1') || ', ' ELSE '' END ||
-        CASE WHEN q4 IS NULL AND r4_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R4_2') || ', ' ELSE '' END ||
-        CASE WHEN q5 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q5') || ', ' ELSE '' END ||
-        CASE WHEN q5 IS NULL AND r5_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R5_1') || ', ' ELSE '' END ||
-        CASE WHEN q5 IS NULL AND r5_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R5_2') || ', ' ELSE '' END ||
-        CASE WHEN q6 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q6') || ', ' ELSE '' END ||
-        CASE WHEN q6 IS NULL AND r6_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R6_1') || ', ' ELSE '' END ||
-        CASE WHEN q6 IS NULL AND r6_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R6_2') || ', ' ELSE '' END ||
-        CASE WHEN q7 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q7') || ', ' ELSE '' END ||
-        CASE WHEN q7 IS NULL AND r7_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R7_1') || ', ' ELSE '' END ||
-        CASE WHEN q7 IS NULL AND r7_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R7_2') || ', ' ELSE '' END ||
-        CASE WHEN q8 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q8') || ', ' ELSE '' END ||
-        CASE WHEN q8 IS NULL AND r8_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R8_1') || ', ' ELSE '' END ||
-        CASE WHEN q8 IS NULL AND r8_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R8_2') || ', ' ELSE '' END ||
-        CASE WHEN q9 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q9') || ', ' ELSE '' END ||
-        CASE WHEN q9 IS NULL AND r9_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R9_1') || ', ' ELSE '' END ||
-        CASE WHEN q9 IS NULL AND r9_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R9_2') || ', ' ELSE '' END ||
-        CASE WHEN q10 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'Q10') || ', ' ELSE '' END ||
-        CASE WHEN q10 IS NULL AND r10_1 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R10_1') || ', ' ELSE '' END ||
-        CASE WHEN q10 IS NULL AND r10_2 IS NOT NULL THEN (SELECT name FROM qareemaster WHERE qkey = 'R10_2') || ', ' ELSE '' END
-    , ', ') AS qarees
+SELECT page_shmrly,description,kholf,group_concat(' ﴾'|| sub_subject || '﴿ ') sub_subject,rasaya,'' AS qarees
 FROM summed_data
-group by page_shmrly, description, kholf, rasaya,qarees
-order by page_shmrly,  rasaya,description, kholf;
+group by page_shmrly,kholf
+order by page_shmrly,kholf
 
 """
 # Execute the SQL query
@@ -155,7 +111,7 @@ file_count = 1
 
 def save_and_create_new_doc():
     global doc, file_count
-    doc.save(f'd:/Qeraat/مشروع العشر/shmrly_imalah_{file_count}.docx')
+    doc.save(f'e:/Qeraat/مشروع العشر/shmrly_imalah_{file_count}.docx')
     file_count += 1
     doc = Document()
 
